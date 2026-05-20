afc793a8acb1e2ce35b76d03b0dd5192f9f7fa7c
Anush <tserunyananush@gmail.com>
2026-05-21 00:40:24 +0400

    should be fixed

--- Compiler/src/Executor.cpp
+++ Compiler/src/Executor.cpp
@@ -13,7 +13,7 @@ std::ifstream Executor::validate(const std::string &exePath)
 	std::getline(f, line);
 	if (line != "~AnushFile")
 		throw std::runtime_error("Invalid file '" + exePath + "'");
-	std::cout << "--Settng up Exection Environment\n";
+	std::cout << "--Settng up Execution Environment\n";
 	memory = new Memory();
 	cpu = new CPU();
 	return f;
@@ -27,7 +27,7 @@ Executor::~Executor()
 
 void Executor::loadAndRun(const std::ifstream &exe)
 {
-	uint16_t nInstructions = Loader::load(exePath, memory, constPool);
+	// uint16_t nInstructions = Loader::load(exePath, memory, constPool);
 	// std::cout << "[VM] Loaded " << nInstructions << " instruction(s) into code section.\n";
 	// std::cout << "[VM] Running...\n\n";
 
